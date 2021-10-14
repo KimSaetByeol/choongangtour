dceaad007f68e2efaf91418804e35994abc9993b
jeeankwon <81234879+jeeankwon@users.noreply.github.com>
2021-10-14 15:30:50 +0900

    Add files via upload

---
+++ choongangtour/src/main/java/com/choongangtour/web/user/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.choongangtour.web.user.controller;
+
+import javax.inject.Inject; 
+import org.slf4j.Logger; 
+import org.slf4j.LoggerFactory; 
+import org.springframework.stereotype.Controller; 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping; 
+ import org.springframework.web.bind.annotation.RequestMethod; 
+ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.choongangtour.web.user.model.UserVO;
+import com.choongangtour.web.user.service.UserService; 
+ 
+ @Controller 
+ @RequestMapping(value = "/user") 
+ public class UserController { 
+	 private static final Logger logger = LoggerFactory.getLogger(UserController.class); 
+	 
+	 @Inject 
+	 private UserService userService; 
+	
+	 @RequestMapping(value = "/getUserList.do", method = RequestMethod.GET) 
+	 public String getUserList(Model model) throws Exception{ 
+		 logger.info("getUserList()...."); 
+		 
+		 model.addAttribute("userList", userService.getUserList()); 
+		 
+		 return "user/userList";
+		 
+	 } 
+	 
+	 @RequestMapping(value = "/insertUser.do", method = RequestMethod.POST) 
+	 public String insertUser(@ModelAttribute("userVO") UserVO userVO , RedirectAttributes rttr) throws Exception { 
+		 
+		 userService.insertUser(userVO); 
+		 
+		 return "redirect:/user/getUserList"; 
+		 
+	}
+}
+
